d934cee495fd547bd15c199c5a963f388c8df86f
jsmith13 <theintangiblefatman@gmail.com>
2019-07-15 20:09:21 -0700

    Masked API keys.

---
+++ sample_config.py
@@ -0,0 +1,8 @@
+## This file holds API keys.
+# To reproduce code, insert the appropriate API keys and rename file to "config.py".
+
+# Hiking Project
+hiking_project_api_key = "your-api-key"
+
+# Mapbox
+mapbox_api_key = "your-api-key"
